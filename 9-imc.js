763dc62255f05a8a3c613ea51f825575b565a455
Fabio <fabio2009pbi@gmail.com>
2023-05-13 23:41:28 -0300

    terminei a atividade de imc com mais detalhes e .toFixed()

--- 9-imc.js
+++ 9-imc.js
@@ -2,18 +2,20 @@ function calculoIMC(peso, altura) {
   let imc = peso / (altura * altura);
 
   if (imc < 18.5) {
-    return `Seu IMC está ${imc.toFixed(1)}, está abaixo do peso`;
+    return `Seu IMC está ${imc.toFixed(1)}, você está abaixo do peso.`;
   } else if (imc >= 18.5 && imc <= 24.9) {
-    return "Peso normal";
+    return `Seu IMC está ${imc.toFixed(1)}, você está com o peso normal.`;
   } else if (imc >= 25 && imc <= 29.9) {
-    return "Acima do peso (Sobrepeso)";
+    return `Seu IMC está ${imc.toFixed(
+      1
+    )}, você está acima do peso(sobrepeso).`;
   } else if (imc >= 30 && imc <= 34.9) {
-    return "Obesidade I";
+    return `Seu IMC está ${imc.toFixed(1)}, você está com Obesidade I.`;
   } else if (imc >= 35 && imc <= 39.9) {
-    return "Obesidade II";
+    return `Seu IMC está ${imc.toFixed(1)}, você está com Obesidade II.`;
   } else {
-    return "Obesidade III";
+    return `Seu IMC está ${imc.toFixed(1)}, você está com Obesidade III.`;
   }
 }
 
-console.log(calculoIMC(50, 1.62));
+console.log(calculoIMC(90, 1.62));
